ee681baa775de3cdedeb593dc4d2adaed923c195
Sivakumar Rajendran <shiva.sms65@gmail.com>
2020-01-28 23:00:50 +0530

    User controller added

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_01_26_110131) do
 
-  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "question_id", null: false
     t.bigint "option_id"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
-  create_table "chapters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "chapters", force: :cascade do |t|
     t.string "name"
     t.bigint "topic_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -32,13 +35,13 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["topic_id"], name: "index_chapters_on_topic_id"
   end
 
-  create_table "exams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "exams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "options", force: :cascade do |t|
     t.text "description"
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["question_id"], name: "index_options_on_question_id"
   end
 
-  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "questions", force: :cascade do |t|
     t.text "description"
     t.bigint "chapter_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -56,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["chapter_id"], name: "index_questions_on_chapter_id"
   end
 
-  create_table "subjects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "subjects", force: :cascade do |t|
     t.string "name"
     t.bigint "exam_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -64,7 +67,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["exam_id"], name: "index_subjects_on_exam_id"
   end
 
-  create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "topics", force: :cascade do |t|
     t.string "name"
     t.bigint "subject_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -72,7 +75,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_110131) do
     t.index ["subject_id"], name: "index_topics_on_subject_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
